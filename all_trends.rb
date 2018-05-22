c24f57ca5f6ab680d17ff31f96e2542a8b885a5f
runwaldarshu <runwaldarshu@gmail.com>
2018-05-22 22:28:09 +0530

    New Data

--- all_trends.rb
+++ all_trends.rb
@@ -18,27 +18,23 @@ def get_timeseries(sym)
 end
 
 def calculate_trends(timeseries, old_sym)
+  high_trends=[]
   # get data for only ,ax last 30 days 
   max = timeseries.high.length < DAYS_TO_TRACK ? (timeseries.high.length - 1) : DAYS_TO_TRACK
   count = 0
   CSV.open("data/#{old_sym}.csv", 'wb') do |csv|
     csv << ['Date', 'Close', 'High', 'Low', 'Change', 'CMP']
     for count in 0...max
-      #p timeseries.close[count+1]
-      #p timeseries.close[count]
-      #p timeseries.high[count]
-      #p timeseries.low[count]
-      #p "#{count} - #{timeseries.high[count]} - #{timeseries.low[count]}"
       # Calculate the trend by comparing difference with closing price  
       close = ((timeseries.close[count][1].to_f - timeseries.close[count+1][1].to_f)/timeseries.close[count+1][1].to_f) * 100
       high = ((timeseries.high[count][1].to_f - timeseries.close[count+1][1].to_f)/timeseries.close[count+1][1].to_f) * 100
       low =  ((timeseries.low[count][1].to_f - timeseries.close[count+1][1].to_f)/timeseries.close[count+1][1].to_f) * 100
-      #p (timeseries.low[count][1].to_f - timeseries.close[count-1][1].to_f)
-      #p "close : #{close}, high : #{high}, low : #{low}, fluctuation : #{high.abs + low.abs}"
-      csv << [timeseries.close[count][0], close, high, low, (high.abs + low.abs), timeseries.close[count][1]]
+      csv << [timeseries.close[count][0], close, high, low, (high.abs + low.abs)]
+      high_trends << high if high > 0.0
       #p "----#{count}-----"
     end
   end
+  high_trends.inject{ |sum, el| sum + el }.to_f / high_trends.size
 end
 
 client = Alphavantage::Client.new key: '1OBOFRNE6KND478D'
@@ -47,6 +43,7 @@ client = Alphavantage::Client.new key: '1OBOFRNE6KND478D'
 text=File.open('nse_fut_list.txt').read
 text.gsub!(/\r\n?/, "\n")
 
+sorted_list = {}
 text.each_line do |sym|
   sym.delete!("\n")
   old_sym = sym
@@ -54,9 +51,12 @@ text.each_line do |sym|
   timeseries = get_timeseries(sym)
   if timeseries
     p timeseries.symbol
-    calculate_trends(timeseries, old_sym)
-
+    avg = calculate_trends(timeseries, old_sym)
+    sorted_list[old_sym] = avg unless avg.nan?
   else
     p "Failed to retrieve data"
   end
 end
+p sorted_list
+sorted_list = sorted_list.sort_by { |name, value| value }
+p sorted_list.to_h.keys
