5c514d29d83a27dae20d58b685dc3e6e776ae505
runwaldarshu <runwaldarshu@gmail.com>
2018-05-27 00:46:49 +0530

    New data and SMA trend

--- all_trends.rb
+++ all_trends.rb
@@ -3,11 +3,12 @@ require 'csv'
 
 DAYS_TO_TRACK=90
 PERCENT_THRESHOLD=8.0
+SECRET_KEY='1OBOFRNE6KND478D'
 
 def get_timeseries(sym)
   for i in 0...5
     begin
-      timeseries = Alphavantage::Timeseries.new symbol: sym, key: "1OBOFRNE6KND478D"
+      timeseries = Alphavantage::Timeseries.new symbol: sym, key: SECRET_KEY
       break
     rescue Alphavantage::Error
       sleep(21)
@@ -15,6 +16,41 @@ def get_timeseries(sym)
     end
   end
   timeseries
+end
+
+def indicator(sym)
+  for i in 0...5
+    begin
+      data = Alphavantage::Indicator.new function: 'SMA', symbol: sym, key: SECRET_KEY, interval: 'daily', time_period: '200', series_type: 'close'
+      break
+    rescue Alphavantage::Error
+      sleep(21)
+      p 'retrying'
+    rescue NoMethodError
+      return nil
+    end
+  end
+  data.hash.to_a[1].to_a[1].to_a if data
+end
+
+def calculate_indicator_trends(indicatorseries, timeseries, old_sym)
+  indicator_trends=[]
+  difference_data=[]
+  for count in 0...10
+    # Calculate the trend by comparing difference with closing price  
+    difference = ((indicatorseries[count][1]['SMA'].to_f - timeseries.close[count][1].to_f)/timeseries.close[count][1].to_f) * 100
+    difference_data << difference.abs
+    indicator_trends << [indicatorseries[count][0], timeseries.close[count][1].to_f, indicatorseries[count][1]['SMA'].to_f, difference]
+  end
+  if difference_data.max < 3
+    CSV.open("indicator_data/#{old_sym}.csv", 'wb') do |csv|
+      csv << ['Date', 'Close', 'SMA', 'Change']
+      indicator_trends.each do |data|
+        csv << data
+      end
+    end
+    return old_sym
+  end
 end
 
 def calculate_trends(timeseries, old_sym)
@@ -37,18 +73,22 @@ def calculate_trends(timeseries, old_sym)
   high_trends.inject{ |sum, el| sum + el }.to_f / high_trends.size
 end
 
-client = Alphavantage::Client.new key: '1OBOFRNE6KND478D'
+#client = Alphavantage::Client.new key: '1OBOFRNE6KND478D'
 #client.verbose = true
 
 text=File.open('nse_fut_list.txt').read
 text.gsub!(/\r\n?/, "\n")
 
 sorted_list = {}
+indicator_list = []
 text.each_line do |sym|
   sym.delete!("\n")
   old_sym = sym
   sym = 'NSE:' + sym
   timeseries = get_timeseries(sym)
+  data = indicator(sym)
+  indicator_data = calculate_indicator_trends(data, timeseries, old_sym) if data
+  indicator_list << old_sym if indicator_data
   if timeseries
     p timeseries.symbol
     avg = calculate_trends(timeseries, old_sym)
@@ -57,6 +97,6 @@ text.each_line do |sym|
     p "Failed to retrieve data"
   end
 end
-p sorted_list
+p indicator_list , "--------"
 sorted_list = sorted_list.sort_by { |name, value| value }
 p sorted_list.to_h.keys
