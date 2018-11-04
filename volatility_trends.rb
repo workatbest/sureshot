b275f39f2024be8da235f41246f4f4ff4de198a4
workatbest <sachin.raka@outlook.com>
2018-11-05 00:54:05 +0530

    Adding monthly data graphs

--- volatility_trends.rb
+++ volatility_trends.rb
@@ -34,19 +34,26 @@ class VolatilityTrends
     return dr_trends.standard_deviation, positive_data.length, (dr_trends.length - positive_data.length)
   end
 
-  def calculate_month_dates
+  def calculate_monthly_data(old_sym, timeseries)
+    monthly_data = []
     start_date = Date.today - DAYS_TO_TRACK
-    binding.pry()
     while(start_date < Date.today)
-      binding.pry()
-      last_day = Date.civil(start_date.year, start_date.month, -1)
-      last_friday = find_date(last_day, 5)
+      last_day_prev_month = Date.civil(start_date.year, start_date.month, -1)
+      data = find_data(find_date(last_day_prev_month, 4), timeseries)
       prev_month = start_date.month == 1 ? 12 : start_date.month - 1
       prev_year = prev_month == 12 ? start_date.year - 1 : start_date.year
-      last_thurday = find_date(Date.civil(prev_year, prev_month, -1), 4)
-      binding.pry()
-      p "#{last_friday.strftime('%Y-%m-%d')}  ---- #{last_thurday.strftime('%Y-%m-%d')}"
-      start_date = last_thurday
+      last_month_data = find_data(find_date(Date.civil(prev_year, prev_month, -1), 4).next, timeseries)
+      #p data, last_month_data
+      monthly_data << ["#{last_month_data['day']}##{data['day']}" , ((data['value'] - last_month_data['value']).abs/last_month_data['value']) * 100.0 ]
+      next_month = start_date.month == 12 ? 1 : start_date.month + 1
+      next_month_year = next_month == 1 ? start_date.year + 1 : start_date.year
+      start_date = Date.civil(next_month_year, next_month,-1)
+    end
+    CSV.open("monthly_data/#{old_sym}.csv", 'wb') do |csv|
+      csv << ['Date', 'Percent']
+      monthly_data.each do |item|
+        csv << item
+      end
     end
   end
 
@@ -57,6 +64,18 @@ class VolatilityTrends
     day
   end
 
+  def find_data(day, timeseries)
+    data = {}
+    while true do
+      if timeseries[day.strftime('%Y-%m-%d')]
+        data['day'] = day.strftime('%Y-%m-%d')
+        data['value'] = timeseries[day.strftime('%Y-%m-%d')]['1. open'].to_f
+        return data
+      end
+      day += 1
+    end
+  end
+
   def volatility
     FileUtils.rm_rf("indicator_data/.", secure: true)
     text=File.open('nse_fut_list.txt').read
@@ -64,6 +83,7 @@ class VolatilityTrends
 
     sorted_list = {}
     indicator_list = {}
+    monthly_data = {}
     text.each_line do |sym|
       sym.delete!("\n")
       old_sym = sym
@@ -72,6 +92,7 @@ class VolatilityTrends
       if timeseries
         p timeseries.symbol
         sorted_list[old_sym] = calculate_daily_returns(timeseries, old_sym)
+        calculate_monthly_data(old_sym, timeseries.hash['Time Series (Daily)'])
       else
         p "Failed to retrieve data"
       end
@@ -88,6 +109,6 @@ class VolatilityTrends
 end
 
 vt = VolatilityTrends.new
-vt.calculate_month_dates
-#vt.volatility
+#vt.calculate_month_dates
+vt.volatility
 
