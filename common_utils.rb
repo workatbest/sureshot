f362ba1f2f5f9c16ebbeb9900bb809d97549b289
workatbest <sachin.raka@outlook.com>
2019-02-10 09:19:43 +0530

    Changes to fix & and search problem

--- common_utils.rb
+++ common_utils.rb
@@ -1,6 +1,7 @@
 require 'alphavantagerb'
 require 'csv'
 require 'fileutils'
+require 'cgi'
 
 SECRET_KEY='1OBOFRNE6KND478D'
 
@@ -8,10 +9,10 @@ class CommonUtils
   def self.get_timeseries(sym, output = 'full')
     for i in 0...5
       begin
-        timeseries = Alphavantage::Timeseries.new symbol: sym, key: SECRET_KEY, outputsize: output
+        timeseries = Alphavantage::Timeseries.new symbol: CGI.escape(sym), key: SECRET_KEY, outputsize: output
         #timeseries = Alphavantage::Timeseries.new symbol: sym, key: SECRET_KEY
         break
-      rescue Alphavantage::Error
+      rescue Alphavantage::Error => e 
         sleep(30)
         p 'retrying'
       end
@@ -22,7 +23,7 @@ class CommonUtils
   def self.indicator_data(sym, indicator, time_period)
     for i in 0...5
       begin
-        data = Alphavantage::Indicator.new  function: indicator, symbol: sym, key: SECRET_KEY, time_period: time_period
+        data = Alphavantage::Indicator.new  function: indicator, symbol: CGI.escape(sym), key: SECRET_KEY, time_period: time_period
         break
       rescue Alphavantage::Error
         sleep(30)
